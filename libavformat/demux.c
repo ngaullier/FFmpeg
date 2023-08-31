b50121ff825003f3081b09e15bd2bed198bd5a83
Nicolas Gaullier <nicolas.gaullier@cji.paris>
2023-08-31 17:06:11 +0200

    [cji] avformat/demux: double probe buffer in estimate_timings_from_pts
    Broadcast optimization, or, at most, safeguard. Should not be required.

--- libavformat/demux.c
+++ libavformat/demux.c
@@ -1831,7 +1831,8 @@ static void estimate_timings_from_bit_rate(AVFormatContext *ic)
                "Estimating duration from bitrate, this may be inaccurate\n");
 }
 
-#define DURATION_DEFAULT_MAX_READ_SIZE 250000LL
+/* 5 video frames @25fps/20Mbps */
+#define DURATION_DEFAULT_MAX_READ_SIZE 500000LL
 #define DURATION_DEFAULT_MAX_RETRY 6
 #define DURATION_MAX_RETRY 1
 
